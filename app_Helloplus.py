1a74392f22aed6c9bd5743d7af5988f33d67cc94
Apparao306 <ambatiapparao@gmail.com>
2024-07-21 20:52:04 -0400

    Update app_Helloplus.py

--- app_Helloplus.py
+++ app_Helloplus.py
@@ -1,11 +1,11 @@
+
 import streamlit as st
-from utils import PrepProcesor, columns 
 
 import numpy as np
 import pandas as pd
-import joblib
+# import joblib
 
-model = joblib.load('xgbpipe.joblib')
+# model = joblib.load('xgbpipe.joblib')
 st.title('Did they survive? :ship:')
 # PassengerId,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 passengerid = st.text_input("Input Passenger ID", '123456') 
